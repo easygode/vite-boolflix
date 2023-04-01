2851409eeade7ce1ebb952595cb1ba5d613dba6f
easygode <bimalbenvenuti@gmail.com>
2023-04-01 20:47:12 +0200

    feature: add poster images

--- src/store.js
+++ src/store.js
@@ -5,7 +5,8 @@ export const store = reactive({
         urlMovie: 'https://api.themoviedb.org/3/search/movie',
         urlTv: 'https://api.themoviedb.org/3/search/tv',
         keyApi: '908af261f8e52211132e82f969d145a4',
-        defaultLang: 'it-IT'
+        defaultLang: 'it-IT',
     },
-    searchKey:''
+    searchKey:'',
+    pathImg: 'https://image.tmdb.org/t/p/w342',
 });
